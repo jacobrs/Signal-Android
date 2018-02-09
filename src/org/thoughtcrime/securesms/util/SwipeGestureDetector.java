40334cb498ee0d9fdbc12dd4494772ddf33e3e6a
Samantha Kerr <samantha-kerr@hotmail.ca>
2018-02-09 14:59:49 -0500

    #10 #17 #15 Added missing content to extracted SwipeGestureDetector class

--- src/org/thoughtcrime/securesms/util/SwipeGestureDetector.java
+++ src/org/thoughtcrime/securesms/util/SwipeGestureDetector.java
@@ -4,5 +4,36 @@ package org.thoughtcrime.securesms.util;
  * Created by Claudia on 2018-02-09.
  */
 
-public class SwipeGestureDetector {
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import org.thoughtcrime.securesms.ConversationActivity;
+
+public class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
+
+    //Min swiping distance & velocity so back swipe isn't triggered accidentally
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    ConversationActivity conversationActivity;
+
+    public SwipeGestureDetector(ConversationActivity conversationActivity) {
+        this.conversationActivity = conversationActivity;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        try {
+            //Difference between the x coordinate of the first even (downpush)
+            // and the second event (let go)
+            float diff = e1.getX() - e2.getX();
+
+            // Right swipe
+            if (-diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                conversationActivity.onRightSwipe();
+            }
+        } catch (Exception e) {
+           // Log.w(TAG, e);
+        }
+        return false;
+    }
 }
