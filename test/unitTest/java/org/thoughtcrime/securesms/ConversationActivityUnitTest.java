390011589b736813109ca1c47830e8ca302e5467
Samantha Kerr <samantha-kerr@hotmail.ca>
2018-02-10 11:43:06 -0500

    #10 #33 #17 Removed unused imports in unit test files

--- test/unitTest/java/org/thoughtcrime/securesms/ConversationActivityUnitTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/ConversationActivityUnitTest.java
@@ -1,28 +1,10 @@
 package org.thoughtcrime.securesms;
 
-import android.util.Log;
-
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.*;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
-
-import java.lang.reflect.Array;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
-import static org.thoughtcrime.securesms.giph.util.InfiniteScrollListener.TAG;
-
-/**
- * Created by Claudia on 2018-02-08.
- */
 
 @RunWith(PowerMockRunner.class)
 public class ConversationActivityUnitTest {
--- test/unitTest/java/org/thoughtcrime/securesms/util/SwipeGesturedetectorUnitTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/util/SwipeGesturedetectorUnitTest.java
@@ -4,14 +4,10 @@ import android.view.MotionEvent;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.thoughtcrime.securesms.ConversationActivity;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.when;
 
