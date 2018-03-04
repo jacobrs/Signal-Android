5a8c93f674751ac9fa4bf4d98ab5bef35e540417
Lori Dalkin <lori.dalkin@gmail.com>
2018-03-03 23:44:26 -0500

    Handled test merge conflicts #75

--- test/unitTest/java/org/thoughtcrime/securesms/database/SmsDatabaseTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/database/SmsDatabaseTest.java
@@ -30,11 +30,15 @@ public class SmsDatabaseTest {
     private long messageId;
     private int testMarkAsUnreadStatus;
     private boolean messageUnread;
+    private int pinnedStatus;
+    private boolean isPinned;
 
     private String tableName;
 
     private String dbMarkUnreadString;
     private String dbRemoveReadReminderString;
+    private String dbPinnedString;
+    private String dbUnpinnedString;
 
     private String[] dbUpdateArgs = new String[2];
 
@@ -53,11 +57,15 @@ public class SmsDatabaseTest {
         threadId = 1111;
         messageId = 1112;
         testMarkAsUnreadStatus = 0;
+        pinnedStatus = 0;
+        isPinned = false;
 
         tableName = "sms";
 
         dbMarkUnreadString = "thread_id = ? AND _id = ? AND read <> 0 AND read_reminder = 0";
         dbRemoveReadReminderString = "thread_id = ? AND _id = ? AND read_reminder <> 0";
+        dbPinnedString = "thread_id = ? AND _id = ? AND pinned = 0";
+        dbUnpinnedString = "thread_id = ? AND _id = ? AND pinned = 1";
 
         dbUpdateArgs[0] = String.valueOf(threadId);
         dbUpdateArgs[1] = String.valueOf(messageId);
@@ -73,6 +81,8 @@ public class SmsDatabaseTest {
     public void resetValues(){
         messageUnread = false;
         testMarkAsUnreadStatus = 0;
+        isPinned = false;
+        pinnedStatus = 0;
     }
 
     @Test
@@ -260,7 +270,92 @@ public class SmsDatabaseTest {
         } catch (Exception e) {
             Log.d(TAG, e.getMessage());
         }
+    }
 
+    @Test
+    public void markMessageAsPinnedTest() {
+        SmsDatabase smsDatabase = new SmsDatabase(mockContext, mockSqlHelper);
+        testContents.put("pinned", 0);
+
+        PowerMockito.doAnswer((Answer) invocation -> {
+            Object[] args = invocation.getArguments();
+
+            //Verifying that the database update call was passed the right arguments
+            assertEquals(tableName, args[0]);
+
+            assert (args[1].getClass().isInstance(ContentValues.class));
+            assertEquals(testContents.get("pinned"), ((ContentValues) args[1]).get("pinned"));
+
+            assertEquals(dbPinnedString, args[2]);
+
+            assert (args[3].getClass().isArray());
+            assertEquals(dbUpdateArgs[0], ((String[]) args[3])[0]);
+            assertEquals(dbUpdateArgs[1], ((String[]) args[3])[1]);
+
+            markAsPinned();
+            return null;
+        }).when(mockSql).update(Matchers.anyString(), Matchers.any(ContentValues.class), Matchers.anyString(), Matchers.any(String[].class));
+
+
+        try {
+            assertEquals(false, isPinned);
+            assertEquals(0, pinnedStatus);
+
+            smsDatabase.markMessagesAsPinned(threadId, messageId);
+
+            PowerMockito.verifyPrivate(smsDatabase).invoke("markMessageAsPinned", dbPinnedString, dbUpdateArgs);
+
+            assertEquals(true, isPinned);
+            assertEquals(1, pinnedStatus);
+
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    @Test
+    public void markMessageAsUnpinnedTest() {
+        SmsDatabase smsDatabase = new SmsDatabase(mockContext, mockSqlHelper);
+        testContents.put("pinned", 1);
+
+        //Set beginning states
+        isPinned = true;
+        pinnedStatus = 1;
+
+        PowerMockito.doAnswer((Answer) invocation -> {
+            Object[] args = invocation.getArguments();
+
+            //Verifying that the database update call was passed the right arguments
+            assertEquals(tableName, args[0]);
+
+            assert (args[1].getClass().isInstance(ContentValues.class));
+            assertEquals(testContents.get("pinned"), ((ContentValues) args[1]).get("pinned"));
+
+            assertEquals(dbUnpinnedString, args[2]);
+
+            assert (args[3].getClass().isArray());
+            assertEquals(dbUpdateArgs[0], ((String[]) args[3])[0]);
+            assertEquals(dbUpdateArgs[1], ((String[]) args[3])[1]);
+
+            markAsUnpinned();
+            return null;
+        }).when(mockSql).update(Matchers.anyString(), Matchers.any(ContentValues.class), Matchers.anyString(), Matchers.any(String[].class));
+
+
+        try {
+            assertEquals(true, isPinned);
+            assertEquals(1, pinnedStatus);
+
+            smsDatabase.markMessagesAsUnpinned(threadId, messageId);
+
+            PowerMockito.verifyPrivate(smsDatabase).invoke("markMessageAsUnpinned", dbPinnedString, dbUpdateArgs);
+
+            assertEquals(false, isPinned);
+            assertEquals(0, pinnedStatus);
+
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage());
+        }
     }
 
     private void markAsUnread(){
@@ -274,6 +369,20 @@ public class SmsDatabaseTest {
         if(messageUnread){
             messageUnread = false;
             testMarkAsUnreadStatus = 0;
+        }
+    }
+
+    private void markAsPinned() {
+        if (!isPinned) {
+            isPinned = true;
+            pinnedStatus = 1;
+        }
+    }
+
+    private void markAsUnpinned() {
+        if (isPinned) {
+            isPinned = false;
+            pinnedStatus = 0;
         }
     }
 }
