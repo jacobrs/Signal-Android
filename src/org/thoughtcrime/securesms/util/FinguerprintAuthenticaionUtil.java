ce4fb0ca3becb4ddecc2d5e5db2e33e4c096aca9
Bryce <bdschoeler@gmail.com>
2018-03-09 21:12:25 -0500

    added fingerprintauth utility method and class #66

---
+++ src/org/thoughtcrime/securesms/util/FinguerprintAuthenticaionUtil.java
@@ -0,0 +1,19 @@
+package org.thoughtcrime.securesms.util;
+
+import android.content.Context;
+import android.os.Build;
+import android.hardware.fingerprint.FingerprintManager;
+/**
+ * Created by bryce on 3/9/2018.
+ */
+
+public class FinguerprintAuthenticaionUtil {
+
+    public static boolean isFinguerprintAuthenticaionSupported(Context context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            FingerprintManager fingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
+            return !fingerprintManager.isHardwareDetected() && !fingerprintManager.hasEnrolledFingerprints();
+        }
+        return false;
+    }
+}
