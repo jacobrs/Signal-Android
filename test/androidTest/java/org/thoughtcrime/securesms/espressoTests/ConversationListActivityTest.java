2cf2df6b3d62798a0cbe08a80c0fb4c6ca6c7888
benrs <benjaminbarault@gmail.com>
2018-03-15 00:04:03 -0400

    Removed unused imports and an unused function, related to #80

--- test/androidTest/java/org/thoughtcrime/securesms/espressoTests/ConversationListActivityTest.java
+++ test/androidTest/java/org/thoughtcrime/securesms/espressoTests/ConversationListActivityTest.java
@@ -4,14 +4,6 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -58,24 +50,5 @@ public class ConversationListActivityTest {
                 allOf(withId(R.id.title), isDisplayed()));
         textView2.check(matches(withText("+15144022093")));
 
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
