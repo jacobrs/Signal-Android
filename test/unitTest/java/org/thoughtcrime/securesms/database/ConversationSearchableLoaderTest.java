345c7711b3694bd65254ee1ad7e9b9b0dadfc541
claudds <claudia.d.s92@gmail.com>
2018-03-16 13:52:56 -0400

    #63 trying to write conversation search loader test. mocks and spies fail due to null pointers and real methods being called even though mocks/spies are being used and stubbed

--- test/unitTest/java/org/thoughtcrime/securesms/database/ConversationSearchableLoaderTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/database/ConversationSearchableLoaderTest.java
@@ -1,8 +1,58 @@
 package org.thoughtcrime.securesms.database;
 
-/**
- * Created by Claudia on 2018-03-16.
- */
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.thoughtcrime.securesms.database.loaders.ConversationSearchableLoader;
+
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DatabaseFactory.class, MmsSmsDatabase.class})
 public class ConversationSearchableLoaderTest {
+
+    ConversationSearchableLoader conversationSearchableLoader;
+    Context mockContext;
+    long threadId;
+    Cursor mockCursor;
+    MmsSmsDatabase mmsSmsDatabase;
+    SQLiteOpenHelper mockSQLhelper;
+
+    @Before
+    public void setup(){
+
+        mockStatic(DatabaseFactory.class);
+        mockContext = mock(Context.class);
+        mockCursor = mock(Cursor.class);
+
+        mockSQLhelper = mock(SQLiteOpenHelper.class);
+        mmsSmsDatabase = new MmsSmsDatabase(mockContext, mockSQLhelper);
+        MmsSmsDatabase spyDb = spy(mmsSmsDatabase);
+        threadId = 1111;
+
+        conversationSearchableLoader = new ConversationSearchableLoader(mockContext, threadId);
+
+        when(DatabaseFactory.getMmsSmsDatabase(mockContext)).thenReturn(spyDb);
+        when(spyDb.getConversation(threadId)).thenReturn(mockCursor);
+
+    }
+
+    @Test
+    public void testGetCursor(){
+
+        Cursor testResult = conversationSearchableLoader.getCursor();
+    }
 }
